f3081665a4343a6494f7cbf9f185654448decb5a
Frode Hoen <frode.hoen@soprasteria.com>
2020-09-19 10:22:05 +0200

    LWC for selecting nav unit in flow

---
+++ force-app/main/default/lwc/nksAssignTask/nksAssignTask.js
@@ -0,0 +1,23 @@
+import { LightningElement, track, api, wire } from 'lwc';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+import getUnits from "@salesforce/apex/NKS_NavUnitsController.getUnits";
+
+export default class NksAssignTask extends LightningElement {
+    @api queueId;
+    @track units;
+    @track queueId = '00G1X000003htJx';
+
+    @wire(getUnits)
+    wiredValues({ data, error }) {
+        if (data) {
+            this.units = JSON.parse(data);
+        } else if (error) {
+            window.console.log('Error in wiredValues');
+            window.console.log(error);
+        }
+    }
+
+    handleUnitChange(event) {
+        this.queueId = event.detail.value;
+    }
+}
