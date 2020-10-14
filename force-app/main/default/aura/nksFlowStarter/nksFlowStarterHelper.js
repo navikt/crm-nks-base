4da8c87594fb6e6dbf420dddde4757dc9e1bd7d5
Stian Ruud Schikora <srs@Stians-MacBook-Pro.local>
2020-10-14 22:23:15 +0200

    Tests and journal API handling

--- force-app/main/default/aura/nksFlowStarter/nksFlowStarterHelper.js
+++ force-app/main/default/aura/nksFlowStarter/nksFlowStarterHelper.js
@@ -1,5 +1,9 @@
 ({
-    helperMethod : function() {
+    isLabelReference: function (component, buttonLabel) {
+        let labelRef = $A.getReference("$Label.c." + buttonLabel);
+        component.set("v.dynamicLabel", labelRef); //Using the dynamic label attribute to async parse label reference
+        let parsedLabel = component.get("v.dynamicLabel");
 
+        return parsedLabel.length !== 0;
     }
 })
