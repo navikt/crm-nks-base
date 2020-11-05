5900be5e3e0d0b5b232b2c039a72878b2f7e8c7d
celinetomren <celine.tomren@soprasteria.com>
2020-11-05 22:35:10 +0100

    add warning badge

--- force-app/main/default/lwc/nksWarningBadges/nksWarningBadges.js
+++ force-app/main/default/lwc/nksWarningBadges/nksWarningBadges.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import getBadges from '@salesforce/apex/NKS_WarningBadgeController.getBadges';
 
-export default class NksWarningBadges extends LightningElement {}
+export default class NksWarningBadges extends LightningElement {
+    @api recordId;
+    @api componentTitle;
+    @track badges;
+    @api objectApiName;
+
+    connectedCallback() {
+        this.getList();
+    }
+
+    get title() {
+        const numRecords = this.relatedRecords ? this.relatedRecords.length : 0;
+        return ' ' + numRecords + ' ' + this.componentTitle;
+    }
+
+    getList() {
+        getBadges({
+            parentId: this.recordId,
+            parentObjectApiName: this.objectApiName
+        })
+            .then(data => {
+                this.badges = (data && data.length > 0) ? data : null;
+            })
+            .catch(error => {
+                console.log('An error occurred: ' + JSON.stringify(error, null, 2));
+            });
+    }
+}
