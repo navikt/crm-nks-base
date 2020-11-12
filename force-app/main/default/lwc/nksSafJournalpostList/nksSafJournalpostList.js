b7f701a71bded1126ae17540382a6421c48f9310
Tor Håkon Sigurdsen <55088511+torhakon@users.noreply.github.com>
2020-11-12 14:07:50 +0100

    La til tester. Justert for named credentials. La til bedre feilhåndtering

--- force-app/main/default/lwc/nksSafJournalpostList/nksSafJournalpostList.js
+++ force-app/main/default/lwc/nksSafJournalpostList/nksSafJournalpostList.js
@@ -1,84 +1,210 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import ACCOUNT_INT_PERSONIDENT from '@salesforce/schema/Account.INT_PersonIdent__c';
-import getJournalposts from "@salesforce/apex/NKS_SafJournalpostListController.getJournalposts";
+import ACCOUNT_INT_ORGANIZATION_NUMBER from '@salesforce/schema/Account.INT_OrganizationNumber__c';
+import ACCOUNT_IS_PERSON_ACCOUNT from '@salesforce/schema/Account.IsPersonAccount';
+import getJournalpostsUser from "@salesforce/apex/NKS_SafJournalpostListController.getJournalpostsUser";
+
+const ACCOUNT_FIELDS = [ACCOUNT_INT_PERSONIDENT, ACCOUNT_INT_ORGANIZATION_NUMBER, ACCOUNT_IS_PERSON_ACCOUNT];
+const DEFAULT_NMB_JOURNALPOSTS = 10;
+const DEFAULT_SELECTED_JOURNALPOST_TYPES = ["I", "U", "N"];
+const DEFAULT_SELECTED_TEMAS = [];
+const QUERY_FIELDS = {
+    name: "journalposter",
+    queryFields: [
+        { name: "journalpostId" },
+        { name: "tittel" },
+        { name: "journalposttype" },
+        { name: "journalstatus" },
+        { name: "tema" },
+        { name: "datoOpprettet" },
+        {
+            name: "sak",
+            queryFields: [
+                { name: "fagsakId" }
+            ]
+        },
+        {
+            name: "avsenderMottaker",
+            queryFields: [
+                { name: "navn" }
+            ]
+        },
+        {
+            name: "dokumenter",
+            queryFields: [
+                { name: "dokumentInfoId" },
+                { name: "tittel" },
+                {
+                    name: "dokumentvarianter",
+                    queryFields: [
+                        { name: "variantformat" },
+                        { name: "filnavn" },
+                        { name: "saksbehandlerHarTilgang" },
+                        { name: "skjerming" },
+                    ]
+                }
+            ]
+        }
+    ]
+};
 
 export default class NksSafJournalpostList extends LightningElement {
     @api recordId;
+    @api selectedJornalpostTypes;
+    @api selectedTemas;
+    @api nmbOfJournalposts;
 
     // tracked resources
     @track journalposts;
-    @track documentOverview;
+    @track filteredJournalPosts;
     @track error;
+
+    // @track queryFields;
+    isLoaded;
+    isLoadingMore;
 
     //Account data that can be used
     wiredAccount;
     account;
 
-    //account parameters used to get journal posts
-    userId;
-    userType;
+    //Query variables
+    queryVariables = {
+        brukerId: {
+            id: "",
+            type: "",
+        },
+        foerste: 10
+    }
 
-    isLoaded = false;
+    connectedCallback() {
+        //Set default variables
+        this.isLoadingMore = false;
+        this.isLoaded = false;
+        this.nmbOfJournalposts = (this.nmbOfJournalposts) ? nmbOfJournalposts : DEFAULT_NMB_JOURNALPOSTS;
+        // this.queryFields = (this.queryFields) ? this.queryFields : DEFAULT_QUERY_FIELDS;
+        this.selectedTemas = (this.selectedTemas) ? this.selectedTemas : DEFAULT_SELECTED_TEMAS;
+        this.selectedJornalpostTypes = (this.selectedJornalpostTypes) ? this.selectedJornalpostTypes : DEFAULT_SELECTED_JOURNALPOST_TYPES;
+        this.journalposts = [];
+        this.filteredJournalPosts = [];
+
+        this.queryVariables.foerste = this.nmbOfJournalposts;
+    }
 
     /**
      * Get the data we need from the account
-     * TODO: Add logic for errorhandling
-     * TODO: Add logic for different account types/ids: orgNmb, ssn or aktorId
      * @param {*} value 
      */
-    @wire(getRecord, { recordId: "$recordId", fields: [ACCOUNT_INT_PERSONIDENT] })
+    @wire(getRecord, { recordId: "$recordId", fields: ACCOUNT_FIELDS })
     getWiredAccount(value) {
         this.wiredAccount = value;
         const { data, error } = value;
 
         if (data) {
+            this.journalposts = [];
+            this.filteredJournalPosts = [];
             this.account = data;
-            this.userId = this.account.fields.INT_PersonIdent__c.value;
-            this.userType = 'AKTOERID';
+            this.setQueryVariablesFromAccount();
+            this.callGetJournalpostsUser();
         }
         else if (error) {
-            //Do something
-        }
-    }
-
-    /**
-     * Get the journal posts related to the account
-     * TODO: Add logic for error handling
-     * @param {*} value 
-     */
-    @wire(getJournalposts, { userId: '$userId', userType: '$userType' })
-    wiredDocumentOverview(value) {
-        const { data, error } = value;
-
-        if (data) {
-            this.journalposts = [];
-            this.error = data.error;
-            this.documentOverview = data.documentOverview;
-
-            if (true === data.isSuccess) {
-                this.journalposts = this.documentOverview.journalposter;
-            }
-            this.isLoaded = true;
-        }
-
-        if (error) {
+            this.setErrorMessage(error, 'fetchResponseError');
             this.isLoaded = true;
         }
     }
 
     /**
-     * Get the error message that we want to display
-     * TODO: Add logic
+     * Set the queryVariables on the BrukerId based on the account
      */
-    get errorMessage() {
-        if (error) {
-            if (401 === error.status) {
-
-            }
-            else {
-                return error.message;
-            }
+    setQueryVariablesFromAccount() {
+        if (this.account.fields.IsPersonAccount.value) {
+            this.queryVariables.brukerId.id = this.account.fields.INT_PersonIdent__c.value;
+            this.queryVariables.brukerId.type = 'AKTOERID';
+        } else {
+            this.queryVariables.brukerId.id = this.account.fields.INT_OrganizationNumber__c.value;
+            this.queryVariables.brukerId.type = 'ORGNR';
         }
+    }
+
+    /**
+     * Get the last journalpostId in the journalpost list and add this to the query variables
+     */
+    queryMoreJournalPosts() {
+        this.isLoadingMore = true;
+        let lastJournalpostId = this.journalposts[this.journalposts.length - 1].journalpostId;
+        this.queryVariables.etter = lastJournalpostId;
+        this.callGetJournalpostsUser();
+    }
+
+    /**
+     * Call getJournalpostsUser apex method and add the new journalposts to the journalpost list
+     */
+    async callGetJournalpostsUser() {
+
+        // Apex can't handle inner classes.
+        // We need to stringify and parse on the other side.
+        const inputParams = {
+            queryVariablesString: JSON.stringify(this.queryVariables),
+            queryFieldString: JSON.stringify(QUERY_FIELDS)
+        };
+
+        this.error = null;
+        try {
+            const journalpostData = await getJournalpostsUser(inputParams);
+
+
+            if (true === journalpostData.isSuccess) {
+                this.journalposts = this.journalposts.concat(journalpostData.documentOverview.journalposter);
+                this.filterAllJournalposts();
+            } else {
+                this.setErrorMessage(journalpostData.error, 'journalpostError');
+            }
+
+        } catch (err) {
+            this.setErrorMessage(err, 'caughtError');
+        }
+
+        this.isLoaded = true;
+        this.isLoadingMore = false;
+    }
+
+    /**
+     * Set filteredJournalPosts to all journals where tema is matching the selected temas (or temalist is empty == alle tema) AND the journalpost type is matching
+     */
+    filterAllJournalposts() {
+        this.filteredJournalPosts = this.journalposts.filter(journalpost => (
+            this.selectedTemas.length > 1 || this.selectedTemas.includes(journalpost.tema))
+            &&
+            (this.selectedJornalpostTypes.includes(journalpost.selectedJornalpostTypes)
+            ));
+    }
+
+    setErrorMessage(error, type) {
+        let errorString;
+
+        switch (type) {
+            case 'fetchResponseError':
+                if (Array.isArray(error.body)) {
+                    errorString = error.body.map(e => e.message).join(', ');
+                } else if (typeof error.body.message === 'string') {
+                    errorString = error.body.message;
+                }
+                break;
+            case 'journalpostError':
+                errorString = '';
+                if (error.status) {
+                    errorString = error.status + ' ';
+                }
+                errorString += error.error + ' - ' + error.message;
+                break;
+            case 'caughtError':
+                errorString = 'Ukjent feil: ' + error.message;
+                break;
+            default:
+                errorString = 'Ukjent feil ' + err;
+
+        }
+
+        this.error = errorString;
     }
 }
