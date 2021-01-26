afa59ec8ec9f368aa562f58d36e14ddeb7d0bf4d
Stian Ruud Schikora <srs@Stians-MacBook-Pro.local>
2021-01-26 21:27:11 +0100

    NAV sak og behandling

---
+++ force-app/main/default/lwc/nksAlertBanner/.doc/README.md
@@ -0,0 +1,10 @@
+## LWC: Alert Banner
+
+LWC for displaying a slds standard [alert banner](https://winter-20.lightningdesignsystem.com/components/alert/)
+
+## Attributes
+
+| Name    | Type   | Access | Required | Description                           |
+| :------ | :----- | :----- | :------- | :------------------------------------ |
+| type    | String | global | true     | Type of banner to display             |
+| message | String | global | true     | Message to be displayed in the banner |
---
+++ force-app/main/default/lwc/nksPersonCaseOverview/nksPersonCaseOverview.js
@@ -0,0 +1,134 @@
+import { LightningElement, api, wire } from 'lwc';
+import getCases from '@salesforce/apex/NKS_NavCaseService.getNavCases';
+import getCategorization from '@salesforce/apex/NKS_ThemeUtils.getCategorization';
+
+//##LABEL IMPORTS
+import NAV_CASE_TITLE from '@salesforce/label/c.NKS_NAV_Case_Title';
+import VALIDATION_ERROR from '@salesforce/label/c.NKS_NAV_Case_Validation_Error';
+import NAV_CASE_RETRIEVE_ERROR from '@salesforce/label/c.NKS_NAV_Case_Retrieve_Error';
+
+export default class NksPersonCaseOverview extends LightningElement {
+
+    @api labels = {
+        NAV_CASE_TITLE,
+        VALIDATION_ERROR,
+        NAV_CASE_RETRIEVE_ERROR
+    }
+
+    @api actorId;
+    caseList = []; //Contains all NAV cases returned from the API
+    displayedCases = []; //Holds the list of cases to be displayed
+    selectedCase;
+    themeGroupOptions = [];
+    filteredThemes = [];
+    themeMap;
+    dataLoaded = false;
+    error = false;
+
+    @wire(getCategorization, {})
+    categoryResults({ data, error }) {
+        if (data) {
+            let themeGroups = [{ label: 'Alle', value: 'ALL' }];
+            let mappedThemes = {};
+
+            data.themeGroups.forEach(themeGroup => {
+                console.log(themeGroup.Name);
+                themeGroups.push({ label: themeGroup.Name, value: themeGroup.Id });
+                //Creating the theme map (ThemegroupId (SF) => [theme CRM_Code__c])
+                let groupThemes = [];
+                if (data.themeMap[themeGroup.Id]) {
+                    groupThemes = data.themeMap[themeGroup.Id].map(theme => {
+                        return theme.CRM_Code__c;
+                    });
+                }
+                mappedThemes[themeGroup.Id] = groupThemes;
+            });
+
+            this.themeGroupOptions = themeGroups;
+            this.themeMap = mappedThemes;
+        }
+        this.dataLoaded = data || error;
+        if (error) this.error = true;
+    }
+
+    @wire(getCases, { actorId: '' })
+    wireUser({
+        error,
+        data
+    }) {
+        if (data) {
+            this.caseList = data;
+            this.displayedCases = this.caseList;
+        }
+        else {
+            console.log(JSON.stringify(error, null, 2));
+        }
+    }
+
+    //Handles the nksNavCaseItem click event and updates the selected attribute for all the childs
+    handleCaseSelected(event) {
+        let selectedNavCaseId = event.detail.selectedCase.saksId;
+        this.selectedCase = event.detail.selectedCase;
+
+        let caseItems = this.template.querySelectorAll('c-nks-nav-case-item');
+        caseItems.forEach(caseItem => {
+            caseItem.selected = caseItem.navCase.saksId == selectedNavCaseId ? true : false;
+        });
+    }
+
+    handleFilterChange(event) {
+        let themeGroup = event.target.value;
+
+        if (themeGroup === 'ALL') {
+            this.displayedCases = this.caseList;
+            return;
+        }
+        else {
+            this.displayedCases = this.caseList.filter(navCase => {
+                return this.themeMap[themeGroup].includes(navCase.sakstema);
+            })
+            console.log(themeGroup);
+        }
+    }
+
+    //Support pagination if 
+    paginate(event) {
+
+    }
+
+    @api
+    get selectedCaseId() {
+        return this.selectedCase ? this.selectedCase.saksId : null;
+    }
+
+    @api
+    get selectedCaseTheme() {
+        return this.selectedCase ? this.selectedCase.sakstema : null;
+    }
+
+    get isLoading() {
+        return this.caseList.length === 0;
+    }
+
+    get title() {
+        return NAV_CASE_TITLE + ' (' + this.caseList.length + ')';
+    }
+
+    get hasCases() {
+        return this.dataLoaded && this.caseList.length !== 0;
+    }
+
+    @api
+    validate() {
+        //Theme and theme group must be set
+        if (this.selectedCase) {
+            return { isValid: true };
+        }
+        else {
+            return {
+                isValid: false,
+                errorMessage: VALIDATION_ERROR
+            };
+        }
+    }
+}
