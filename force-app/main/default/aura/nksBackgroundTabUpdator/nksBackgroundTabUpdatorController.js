68ffca5678b0c37de8d21b90da7f109dabda1fd4
celinetomren <celine.tomren@soprasteria.com>
2021-01-26 08:18:52 +0100

    only focus on main tabs

--- force-app/main/default/aura/nksBackgroundTabUpdator/nksBackgroundTabUpdatorController.js
+++ force-app/main/default/aura/nksBackgroundTabUpdator/nksBackgroundTabUpdatorController.js
@@ -2,9 +2,15 @@
     onTabCreated: function (component, event, helper) {
         var newTabId = event.getParam('tabId');
         var workspace = component.find("workspace");
-        workspace.focusTab({
+        workspace.isSubtab({
             tabId: newTabId
-        })
+        }).then(function (response) {
+            if (!response) {
+                workspace.focusTab({
+                    tabId: newTabId
+                })
+            }
+        });
         workspace.getTabInfo({
             tabId: newTabId
         }).then(function (response) {
@@ -26,7 +32,7 @@
         var omniAPI = component.find("omniToolkit");
         var action = component.get("c.getOnlineId");
         action.setCallback(this, function (data) {
-            if (data.getReturnValue().length > 0) {
+            if (data.getReturnValue() != null && data.getReturnValue().length > 0) {
                 var poll = setInterval(function () {
                     omniAPI.login({ statusId: data.getReturnValue() }).then(function (result) {
                         clearInterval(poll);
