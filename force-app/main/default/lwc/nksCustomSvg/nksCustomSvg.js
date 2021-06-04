ebb8f3d15a629a1ba059055167513111f48521d5
Tor Håkon Sigurdsen <55088511+torhakon@users.noreply.github.com>
2021-06-04 16:57:13 +0200

    Custom SVG component (#524)

---
+++ force-app/main/default/lwc/nksCustomSvg/nksCustomSvg.js
@@ -0,0 +1,23 @@
+import { LightningElement, api } from 'lwc';
+
+export default class NksCustomSvg extends LightningElement {
+    @api size;
+    @api altText;
+    @api src;
+
+    get iconClasses() {
+        let styleClasses = 'slds-icon slds-icon-text-default';
+        switch (this.size) {
+            case 'xx-small':
+                return styleClasses + ' slds-icon_xx-small';
+            case 'x-small':
+                return styleClasses + ' slds-icon_x-small';
+            case 'small':
+                return styleClasses + ' slds-icon_small';
+            case 'large':
+                return styleClasses + ' slds-icon_large';
+            default:
+                return styleClasses;
+        }
+    }
+}
