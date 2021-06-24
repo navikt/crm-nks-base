6aea0e2763bc5509097dda3cd5d4ed3ebaf5f9e2
Tor Håkon Sigurdsen <55088511+torhakon@users.noreply.github.com>
2021-06-24 19:31:46 +0200

    NAdd new Aura component Flow Modal

---
+++ force-app/main/default/aura/nksFlowModal/nksFlowModalController.js
@@ -0,0 +1,34 @@
+({
+    openModel: function (component, event, helper) {
+        // Set isModalOpen attribute to true
+
+        var params = event.getParam('arguments');
+        if (params) {
+            component.set('v.isModalOpen', true);
+            component.set('v.modalAriaLabel', params.modalAriaLabel);
+
+            const flow = component.find('flowData');
+
+            let input = [
+                {
+                    name: 'recordId',
+                    type: 'String',
+                    value: component.get('v.recordId')
+                }
+            ];
+            flow.startFlow(params.flowToOpen, input);
+        }
+    },
+
+    closeModel: function (component, event, helper) {
+        // Set isModalOpen attribute to false
+        component.set('v.isModalOpen', false);
+    },
+
+    flowStatusChange: function (component, event, helper) {
+        let flowStatus = event.getParam('status');
+        if (flowStatus === 'FINISHED' || flowStatus === 'FINISHED_SCREEN') {
+            component.set('v.isModalOpen', false);
+        }
+    }
+});
