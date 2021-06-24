877f8eb4964c9be05d441ffac5a69253c48eeaa2
Tor Håkon Sigurdsen <55088511+torhakon@users.noreply.github.com>
2021-06-24 19:32:22 +0200

    Custom Svg - updates

--- force-app/main/default/lwc/nksCustomSvg/nksCustomSvg.js
+++ force-app/main/default/lwc/nksCustomSvg/nksCustomSvg.js
@@ -4,10 +4,12 @@ export default class NksCustomSvg extends LightningElement {
     @api size;
     @api altText;
     @api src;
+    @api classes;
+    @api viewBox;
 
     get iconClasses() {
-        console.log(this.src);
         let styleClasses = 'slds-icon slds-icon-text-default';
+        styleClasses += this.classes ? ' ' + this.classes : '';
         switch (this.size) {
             case 'xx-small':
                 return styleClasses + ' slds-icon_xx-small';
