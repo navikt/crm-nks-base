e29f78722107d48e616ff0eed06f0f7009f8a331
Eugenijus <eugenijus.margalikas@nav.no>
2021-09-27 10:06:40 +0200

    created relation lwc component

---
+++ force-app/main/default/lwc/relationFromPdlViewer/relationFromPdlViewer.js
@@ -0,0 +1,30 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+import getRelatedPersons from '@salesforce/apex/FamilyRelationPDLViewerController.getRelatedPersons';
+
+export default class RelationFromPdlViewer extends LightningElement {
+    @api objectApiName;
+    @api recordId;
+    wireFields;
+
+    connectedCallback() {
+        this.wireFields = [this.objectApiName + '.Id'];
+    }
+
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: '$wireFields'
+    })
+    wiredRecordInfo({ data }) {
+        if (data) {
+            refreshApex(this.relations);
+        }
+    }
+
+    @wire(getRelatedPersons, {
+        recordId: '$recordId',
+        objectApiName: '$objectApiName'
+    })
+    relations;
+}
