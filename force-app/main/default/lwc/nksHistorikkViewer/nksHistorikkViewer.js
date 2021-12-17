cf1a67d5e13b0b016cf9286334b75f31f5013aa9
Ibha Gupta <ibha.gupta@nav.no>
2021-12-17 14:13:50 +0100

    commit

--- force-app/main/default/lwc/nksHistorikkViewer/nksHistorikkViewer.js
+++ force-app/main/default/lwc/nksHistorikkViewer/nksHistorikkViewer.js
@@ -1,8 +1,4 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
-import { refreshApex } from '@salesforce/apex';
-import getHistorikk from '@salesforce/apex/NKS_HistorikkViewController.getHistorikk';
-
+import { LightningElement, api } from 'lwc';
 export default class NksHistorikkViewer extends LightningElement {
     @api objectApiName;
     @api recordId;
