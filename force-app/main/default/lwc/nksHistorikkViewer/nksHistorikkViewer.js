525d0555b3b47170a414ab59d69268b60228817c
Ibha Gupta <ibha.gupta@nav.no>
2022-01-05 14:09:12 +0100

    fullmakt_master1

---
+++ force-app/main/default/lwc/nksHistorikkViewer/nksHistorikkViewer.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+
+export default class NksHistorikkViewer extends LightningElement {
+    @api objectApiName;
+    @api recordId;
+    @api fullmaktData;
+}
