f632f11da771ceaa4b7a451183b93615ff1df5ca
SRS <stian.schikora@nav.no>
2022-01-14 09:31:23 +0100

    Removed unnecessary console log

--- force-app/main/default/aura/knowledgeSummaryCopy/knowledgeSummaryCopyHelper.js
+++ force-app/main/default/aura/knowledgeSummaryCopy/knowledgeSummaryCopyHelper.js
@@ -2,7 +2,6 @@
     addToClipBoard: function (component, text) {
         let escaped = text.replaceAll(/<br>/g, '\n');
 
-        console.log('ESCAPED: ' + escaped);
         const doc = new DOMParser().parseFromString(escaped, 'text/html'); //Handling correct parsing when copying text
 
         var hiddenInput = document.createElement('textarea');
