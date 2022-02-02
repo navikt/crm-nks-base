2113130b9cc2f27e0339bf90304c5d633847ffc6
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2022-02-02 12:44:27 +0100

    Added list entry + test + dummy data

--- force-app/main/default/lwc/nksLinksReadyResponse/nksLinksReadyResponse.js
+++ force-app/main/default/lwc/nksLinksReadyResponse/nksLinksReadyResponse.js
@@ -2,7 +2,7 @@ import { LightningElement, api, track } from 'lwc';
 import getReadyResponse from '@salesforce/apex/NKS_HomePageController.getReadyResponse';
 import { NavigationMixin } from 'lightning/navigation';
 
-export default class nksLinksReadyResponse extends LightningElement {
+export default class nksLinksReadyResponse extends NavigationMixin(LightningElement) {
     @api showReadyResponse;
     @track records;
 
