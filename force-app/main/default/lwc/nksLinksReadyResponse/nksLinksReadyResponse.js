b6ff094f8b6bc129950471bb749a98da7be2701d
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2022-03-01 12:52:17 +0100

    Updated branch

--- force-app/main/default/lwc/nksLinksReadyResponse/nksLinksReadyResponse.js
+++ force-app/main/default/lwc/nksLinksReadyResponse/nksLinksReadyResponse.js
@@ -1,12 +1,11 @@
 import { LightningElement, api, track } from 'lwc';
 import getReadyResponse from '@salesforce/apex/NKS_HomePageController.getReadyResponse';
-import { NavigationMixin } from 'lightning/navigation';
-
-export default class nksLinksReadyResponse extends NavigationMixin(LightningElement) {
-    @api showReadyResponse;
+export default class NksLinksReadyResponse extends LightningElement {
     @track records = [];
 
     isInitiated = false;
+    size;
+    className;
 
     connectedCallback() {
         this.isInitiated = true;
@@ -24,6 +23,14 @@ export default class nksLinksReadyResponse extends NavigationMixin(LightningElem
     }
 
     get showReadyResponse() {
-        return this.records.length > 0 ? true : false;
+        if (this.records.length > 0) {
+            this.size = 7;
+            this.className = 'slds-var-p-left_large';
+            return true;
+        } else {
+            this.size = 12;
+            this.className = '';
+            return false;
+        }
     }
 }
