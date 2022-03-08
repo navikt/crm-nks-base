13c05977c657e5c3b480b556d803df167b381f29
SRS <stian.schikora@nav.no>
2022-03-08 12:10:13 +0100

    Revert "chat-aura-components". IN chat mover

---
+++ force-app/main/default/aura/nksChatAuthenticationInfoContainer/nksChatAuthenticationInfoContainerController.js
@@ -0,0 +1,38 @@
+({
+    //Handles event from LWC to init the auth process using the conversation toolkit API
+    requestAuthentication: function (component, event, helper) {
+        const chatToolkit = component.find('chatToolkit');
+        const recordId = component.get('v.recordId');
+        const authInfoCmp = component.find('chatAuthInfo');
+        let authUrl = event.getParam('authUrl');
+
+        chatToolkit
+            .sendMessage({
+                recordId: recordId,
+                message: {
+                    text: 'Init:Auth:' + authUrl + recordId
+                }
+            })
+            .then(function (result) {
+                //Call child to handle message result
+                authInfoCmp.authRequestHandling(result);
+            });
+    },
+
+    showLoginMsg: function (component, event, helper) {
+        const chatToolkit = component.find('chatToolkit');
+        const recordId = component.get('v.recordId');
+        const loginMsg = event.getParam('loginMessage');
+
+        chatToolkit
+            .sendMessage({
+                recordId: recordId,
+                message: {
+                    text: loginMsg
+                }
+            })
+            .then(function (result) {
+                //Message success
+            });
+    }
+});
