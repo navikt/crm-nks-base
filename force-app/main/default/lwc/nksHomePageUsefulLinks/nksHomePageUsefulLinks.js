9053f8ce21e13752041d140f2585b30dc9f60bce
Morten Mikalsen <morten.mikalsen@hotmail.com>
2022-05-11 14:48:26 +0200

    Changes sikkerhetsrutine link to not open new tab, also UU fixes

--- force-app/main/default/lwc/nksHomePageUsefulLinks/nksHomePageUsefulLinks.js
+++ force-app/main/default/lwc/nksHomePageUsefulLinks/nksHomePageUsefulLinks.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
-
-export default class NksHomePageUsefulLinks extends LightningElement {}
+import { NavigationMixin } from 'lightning/navigation';
+export default class NksHomePageUsefulLinks extends NavigationMixin(LightningElement) {
+    test() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__knowledgeArticlePage',
+            attributes: {
+                actionName: 'view',
+                articleType: 'Knowledge',
+                urlName: encodeURIComponent('Felles-Trussel-trakassering-og-sjikane')
+            }
+        });
+    }
+}
