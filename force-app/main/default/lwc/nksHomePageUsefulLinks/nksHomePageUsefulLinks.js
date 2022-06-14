430fcd8eacaf2f5ee3a90be14e70bb0f061661d5
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2022-06-14 16:27:22 +0200

    Added Svarberedskap

--- force-app/main/default/lwc/nksHomePageUsefulLinks/nksHomePageUsefulLinks.js
+++ force-app/main/default/lwc/nksHomePageUsefulLinks/nksHomePageUsefulLinks.js
@@ -1,6 +1,43 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import getReadyResponse from '@salesforce/apex/NKS_HomePageController.getReadyResponses';
+
 export default class NksHomePageUsefulLinks extends NavigationMixin(LightningElement) {
+    @track records = [];
+
+    isInitiated = false;
+    size;
+    className;
+
+    connectedCallback() {
+        this.isInitiated = true;
+        this.loadList();
+    }
+
+    loadList() {
+        getReadyResponse()
+            .then((result) => {
+                this.records = result;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    get showReadyResponse() {
+        let show;
+        if (this.records.length > 0) {
+            this.size = 7;
+            this.className = 'slds-var-p-left_large';
+            show = true;
+        } else {
+            this.size = 12;
+            this.className = '';
+            show = false;
+        }
+        return show;
+    }
+
     navigateToKnowledge() {
         this[NavigationMixin.Navigate]({
             type: 'standard__knowledgeArticlePage',
