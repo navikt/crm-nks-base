65465f71a57612b0d16e09108dbfbddf10763cfb
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2022-06-16 11:41:13 +0200

    Removed unrelated changes

--- force-app/main/default/lwc/nksHomePageText/nksHomePageText.js
+++ force-app/main/default/lwc/nksHomePageText/nksHomePageText.js
@@ -1,24 +1,18 @@
 import { LightningElement, api } from 'lwc';
 import getField from '@salesforce/apex/NKS_HomePageController.getField';
-import { subscribe, onError } from 'lightning/empApi';
 
 export default class NksHomePageText extends LightningElement {
     @api cardTitle;
     @api iconName;
     @api type;
 
-    text = '';
-    channelName = '/topic/Announcement_Updates';
-    subscription = {};
-    isInitiated = false;
+    @api text;
 
     connectedCallback() {
-        this.isInitiated = true;
-        this.loadField();
-        this.handleSubscribe();
+        this.getField();
     }
 
-    loadField() {
+    getField() {
         getField({
             type: this.type
         })
@@ -32,31 +26,8 @@ export default class NksHomePageText extends LightningElement {
 
     get icon() {
         let nameString = null;
-        if (this.iconName && this.iconName !== '') nameString = this.iconName;
+        if (this.iconName && this.iconName != '') nameString = this.iconName;
 
         return nameString;
     }
-
-    get isEmpty() {
-        return this.text === null || this.text === '' ? true : false;
-    }
-
-    get isOperational() {
-        return this.type === 'Teknisk og drift' ? true : false;
-    }
-
-    handleSubscribe() {
-        subscribe(this.channelName, -1, this.refreshField).then((response) => {
-            console.log('Subscription request sent to: ', JSON.stringify(response.channel));
-            this.subscription = response;
-        });
-        onError((error) => {
-            console.error('Received error from server: ', JSON.stringify(error));
-        });
-    }
-
-    refreshField = () => {
-        this.isInitiated = true;
-        this.loadField();
-    };
 }
