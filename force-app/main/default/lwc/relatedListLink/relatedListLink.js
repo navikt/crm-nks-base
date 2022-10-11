a0590dceea2f48bf800d0a1d614322be3bd56ded
Morten Mikalsen <morten.mikalsen@hotmail.com>
2022-10-11 16:31:52 +0200

    Add related list link

---
+++ force-app/main/default/lwc/relatedListLink/relatedListLink.js
@@ -0,0 +1,38 @@
+import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+
+export default class RelatedListLink extends NavigationMixin(LightningElement) {
+    @api recordId;
+    @api objectApiName;
+    @api label;
+    @api relatedList;
+    @api relationshipField;
+
+    wireFields;
+    relatedListUrl;
+
+    connectedCallback() {
+        this.wireFields = this.relationshipField ? [this.relationshipField] : [this.objectApiName + '.Id'];
+    }
+
+    @wire(getRecord, { recordId: '$recordId', fields: '$wireFields' })
+    wiredLink({ data, error }) {
+        if (error) {
+            console.log(error);
+        }
+        if (data) {
+            const value = this.relationshipField ? getFieldValue(data, this.relationshipField) : this.recordId;
+            this[NavigationMixin.GenerateUrl]({
+                type: 'standard__recordRelationshipPage',
+                attributes: {
+                    recordId: value,
+                    relationshipApiName: this.relatedList,
+                    actionName: 'view'
+                }
+            })
+                .then((url) => (this.relatedListUrl = url))
+                .catch((a) => console.log(a));
+        }
+    }
+}
