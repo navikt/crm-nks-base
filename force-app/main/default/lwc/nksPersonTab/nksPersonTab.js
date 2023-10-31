b8d624e54defcb9f2d32fe95185954a58f85ea9e
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2023-10-31 13:03:09 +0100

    added label

--- force-app/main/default/lwc/nksPersonTab/nksPersonTab.js
+++ force-app/main/default/lwc/nksPersonTab/nksPersonTab.js
@@ -1,8 +1,21 @@
 import { LightningElement, api } from 'lwc';
+import DETAILS_TAB_LABEL from '@salesforce/label/c.NKS_Details_Tab_Label';
+import COMMUNICATION_TAB_LABEL from '@salesforce/label/c.NKS_Communication_Tab_Label';
+import TASKS_TAB_LABEL from '@salesforce/label/c.NKS_Tasks_Tab_Label';
+import DOCUMENTS_TAB_LABEL from '@salesforce/label/c.NKS_Documents_Tab_Label';
+import PAYOUT_TAB_LABEL from '@salesforce/label/c.NKS_Payout_Tab_Label';
+import CASES_TAB_LABEL from '@salesforce/label/c.NKS_Cases_Tab_Label';
 
 export default class NksPersonTab extends LightningElement {
     @api recordId;
     @api objectApiName;
+
+    details = DETAILS_TAB_LABEL;
+    communication = COMMUNICATION_TAB_LABEL;
+    tasks = TASKS_TAB_LABEL;
+    documents = DOCUMENTS_TAB_LABEL;
+    payout = PAYOUT_TAB_LABEL;
+    cases = CASES_TAB_LABEL;
 
     // TODO: Add logic for both account and case
     // TODO: Use labels for english/norwegian
