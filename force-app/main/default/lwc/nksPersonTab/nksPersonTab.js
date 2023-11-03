676c5e543bffd9285e144270267577892b7a086a
Morten Mikalsen <morten.mikalsen@hotmail.com>
2023-11-03 12:07:05 +0100

    Adds conditional rendering

--- force-app/main/default/lwc/nksPersonTab/nksPersonTab.js
+++ force-app/main/default/lwc/nksPersonTab/nksPersonTab.js
@@ -1,15 +1,20 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import DETAILS_TAB_LABEL from '@salesforce/label/c.NKS_Details_Tab_Label';
 import COMMUNICATION_TAB_LABEL from '@salesforce/label/c.NKS_Communication_Tab_Label';
 import TASKS_TAB_LABEL from '@salesforce/label/c.NKS_Tasks_Tab_Label';
 import DOCUMENTS_TAB_LABEL from '@salesforce/label/c.NKS_Documents_Tab_Label';
 import PAYOUT_TAB_LABEL from '@salesforce/label/c.NKS_Payout_Tab_Label';
 import CASES_TAB_LABEL from '@salesforce/label/c.NKS_Cases_Tab_Label';
+import CASE_ACCOUNT_FIELD from '@salesforce/schema/Case.AccountId';
+import ACCOUNT_ID_FIELD from '@salesforce/schema/Account.Id';
+import ACCOUNT_PERSON_FIELD from '@salesforce/schema/Account.CRM_Person__c';
+import MOVED_FIELD from '@salesforce/schema/Person__c.INT_MovedToCountry__c';
+import PERSON_ACTORID_FIELD from '@salesforce/schema/Person__c.INT_ActorId__c';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 export default class NksPersonTab extends LightningElement {
     @api recordId;
     @api objectApiName;
-    personId = 'a0F1w000004cB1EEAU';
     relatedListDisabled = false;
     relatedListHeading = '';
 
@@ -21,7 +26,23 @@ export default class NksPersonTab extends LightningElement {
     payout = PAYOUT_TAB_LABEL;
     cases = CASES_TAB_LABEL;
 
-    // TODO: Add logic for both account and case
+    personId;
+    accountId;
+    accountField;
+    actorId;
+    movedCountry;
+
+    connectedCallback() {
+        if (this.objectApiName === 'Case') {
+            this.accountField = [CASE_ACCOUNT_FIELD];
+            return;
+        }
+        if (this.objectApiName === 'Account') {
+            this.accountId = this.recordId;
+            this.accountField = [ACCOUNT_ID_FIELD];
+        }
+    }
+
     get relationshipField() {
         if (this.objectApiName === 'Case') {
             return 'Account.CRM_Person__c';
@@ -40,14 +61,59 @@ export default class NksPersonTab extends LightningElement {
         return null;
     }
 
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: '$accountField'
+    })
+    wiredRecordInfo({ error, data }) {
+        if (data) {
+            if (this.accountField) {
+                this.accountId = getFieldValue(data, this.accountField[0]);
+            }
+        }
+        if (error) {
+            console.log('error: ');
+            console.log(error);
+        }
+    }
+
+    @wire(getRecord, {
+        recordId: '$accountId',
+        fields: [ACCOUNT_PERSON_FIELD]
+    })
+    wiredAccountInfo({ error, data }) {
+        if (data) {
+            this.personId = getFieldValue(data, ACCOUNT_PERSON_FIELD);
+        }
+
+        if (error) {
+            console.log('error: ');
+            console.log(error);
+        }
+    }
+
+    @wire(getRecord, {
+        recordId: '$personId',
+        fields: [PERSON_ACTORID_FIELD, MOVED_FIELD]
+    })
+    wiredPersonInfo({ error, data }) {
+        if (data) {
+            this.actorId = getFieldValue(data, PERSON_ACTORID_FIELD);
+            this.movedCountry = getFieldValue(data, MOVED_FIELD);
+        }
+
+        if (error) {
+            console.log('error:');
+            console.log(error);
+        }
+    }
+
     get tabConditional() {
-        // TODO: Add conditions for show any information, mostly access
-        return true;
+        return this.actorId != null;
     }
 
     get flyttingConditional() {
-        // TODO: Add conditions for hiding flytting, mostly access
-        return true;
+        return this.movedCountry != null;
     }
 
     handleTabClick(event) {
