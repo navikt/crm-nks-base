cb154f1c90fbf77948a2da769b626bdd268a28ed
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2023-11-14 09:06:50 +0100

    all file changes (some functions do not work yet)

--- force-app/main/default/lwc/nksSurvey/nksSurvey.js
+++ force-app/main/default/lwc/nksSurvey/nksSurvey.js
@@ -1,60 +1,53 @@
 import { LightningElement, track, wire } from 'lwc';
-import PNG_EMOJIS from '@salesforce/resourceUrl/nksSurveyEmojis';
+import SVG_EMOJIS from '@salesforce/resourceUrl/nksSurveyEmojis';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getSurvey from '@salesforce/apex/NKS_InternalSurveyController.getSurvey';
 import hasAnswered from '@salesforce/apex/NKS_InternalSurveyController.hasAnswered';
-import { createRecord, getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import SURVEY_RESPONSE_OBJECT from '@salesforce/schema/NKS_Survey_Response__c';
-import SURVEY_FIELD from '@salesforce/schema/NKS_Survey_Response__c.NKS_Survey__c';
-import RATING_FIELD from '@salesforce/schema/NKS_Survey_Response__c.NKS_Rating__c';
-import COMMENT_FIELD from '@salesforce/schema/NKS_Survey_Response__c.NKS_Comment__c';
-import RECORD_TYPE_ID_FIELD from '@salesforce/schema/NKS_Survey_Response__c.RecordTypeId';
-import USER_FIELD from '@salesforce/schema/NKS_Survey_Response__c.User__c';
-import ANSWERED_FIELD from '@salesforce/schema/NKS_Survey_Response__c.NKS_Answered__c';
-import USER_ID from '@salesforce/user/Id';
+import createAnsweredRecord from '@salesforce/apex/NKS_InternalSurveyController.createAnsweredRecord';
+import CreateResponseRecord from '@salesforce/apex/NKS_InternalSurveyController.CreateResponseRecord';
 
 export default class NksSurvey extends LightningElement {
     surveyId;
     backgroundColor;
     title;
     question;
-    userId = USER_ID;
     isAnswered;
+    isRendered = false;
+    recordTypeId;
 
     @track emojis = [
         {
             id: 'emoji1',
             title: 'veldig dårlig',
-            url: `${PNG_EMOJIS}/emoji1.png`,
+            url: `${SVG_EMOJIS}/emoji1.svg`,
             selected: false,
             value: 1
         },
         {
             id: 'emoji2',
             title: 'dårlig',
-            url: `${PNG_EMOJIS}/emoji2.png`,
+            url: `${SVG_EMOJIS}/emoji2.svg`,
             selected: false,
             value: 2
         },
         {
             id: 'emoji3',
             title: 'nøytral',
-            url: `${PNG_EMOJIS}/emoji3.png`,
+            url: `${SVG_EMOJIS}/emoji3.svg`,
             selected: false,
             value: 3
         },
         {
             id: 'emoji4',
             title: 'bra',
-            url: `${PNG_EMOJIS}/emoji4.png`,
+            url: `${SVG_EMOJIS}/emoji4.svg`,
             selected: false,
             value: 4
         },
         {
             id: 'emoji5',
             title: 'veldig bra',
-            url: `${PNG_EMOJIS}/emoji5.png`,
+            url: `${SVG_EMOJIS}/emoji5.svg`,
             selected: false,
             value: 5
         }
@@ -62,9 +55,6 @@ export default class NksSurvey extends LightningElement {
     @track rating;
     @track comment;
     @track show = true;
-
-    @wire(getObjectInfo, { objectApiName: SURVEY_RESPONSE_OBJECT })
-    objectInfo;
 
     @wire(getSurvey)
     wiredSurvey({ data, error }) {
@@ -86,9 +76,6 @@ export default class NksSurvey extends LightningElement {
         return !this.show || this.isAnswered;
     }
 
-    /**
-     * Note: some functions are for test
-     */
     renderedCallback() {
         if (this.surveyId) {
             hasAnswered({ surveyId: this.surveyId }).then((res) => {
@@ -98,29 +85,17 @@ export default class NksSurvey extends LightningElement {
         }
     }
 
-    getRecordTypeId(recordTypeName) {
-        const rtis = this.objectInfo.data.recordTypeInfos;
-        return Object.keys(rtis).find((rti) => rtis[rti].name === recordTypeName);
-    }
-
-    handleCloseClick() {
-        this.show = false;
-    }
-
     handleClick(event) {
         const id = event.currentTarget.getAttribute('data-id');
         this.emojis.forEach((emoji) => {
             let element = this.template.querySelector(`img[data-id="${emoji.id}"]`);
-            let item = this.template.querySelector(`li[data-id="${emoji.id}"]`);
             if (emoji.id === id) {
-                element.setAttribute('src', `${PNG_EMOJIS}/${emoji.id}hover.png`);
+                element.setAttribute('src', `${SVG_EMOJIS}/${emoji.id}select.svg`);
                 emoji.selected = true;
-                item.className = 'slds-box slds-box_small emojiSelected';
                 this.rating = event.currentTarget.getAttribute('value');
             } else {
-                element.setAttribute('src', `${PNG_EMOJIS}/${emoji.id}.png`);
+                element.setAttribute('src', `${SVG_EMOJIS}/${emoji.id}.svg`);
                 emoji.selected = false;
-                item.className = 'slds-box slds-box_small emoji';
             }
         });
     }
@@ -128,7 +103,7 @@ export default class NksSurvey extends LightningElement {
     handleMouseOver(event) {
         const id = event.currentTarget.getAttribute('data-id');
         let element = this.template.querySelector(`img[data-id="${id}"]`);
-        element.setAttribute('src', `${PNG_EMOJIS}/${id}hover.png`);
+        element.setAttribute('src', `${SVG_EMOJIS}/${id}hover.svg`);
     }
 
     handleMouseOut(event) {
@@ -136,7 +111,7 @@ export default class NksSurvey extends LightningElement {
         this.emojis.forEach((emoji) => {
             let element = this.template.querySelector(`img[data-id="${emoji.id}"]`);
             if (emoji.id === id && !emoji.selected) {
-                element.setAttribute('src', `${PNG_EMOJIS}/${id}.png`);
+                element.setAttribute('src', `${SVG_EMOJIS}/${id}.svg`);
             }
         });
     }
@@ -146,8 +121,6 @@ export default class NksSurvey extends LightningElement {
     }
 
     handleSend() {
-        this.createAnsweredRecord();
-        this.createResponseRecord();
         this.show = false;
         const event = new ShowToastEvent({
             title: 'Tilbakemeldingen din er mottatt.',
@@ -155,46 +128,20 @@ export default class NksSurvey extends LightningElement {
             variant: 'success'
         });
         this.dispatchEvent(event);
-    }
-
-    handleCancel() {
-        this.createAnsweredRecord();
-        this.show = false;
-    }
-
-    createAnsweredRecord() {
-        const answeredFields = {};
-
-        answeredFields[SURVEY_FIELD.fieldApiName] = this.surveyId;
-        answeredFields[RECORD_TYPE_ID_FIELD.fieldApiName] = this.getRecordTypeId('Survey Answered');
-        answeredFields[USER_FIELD.fieldApiName] = this.userId;
-        answeredFields[ANSWERED_FIELD.fieldApiName] = true;
-
-        const recordInput = {
-            apiName: SURVEY_RESPONSE_OBJECT.objectApiName,
-            fields: answeredFields
-        };
-
-        createRecord(recordInput).then((record) => {
-            console.log('SurveyAnswered is created: ', record);
+        createAnsweredRecord({ surveyId: this.surveyId }).then((res) => {
+            console.log('Result of Survey Answered creation: ', res);
+            console.log('rating from lwc: ', this.rating);
+            console.log('comment from lwc: ', this.comment);
+            CreateResponseRecord({ rating: this.rating, comment: this.comment }).then((result) => {
+                console.log('Result of Survey Response creation: ', result);
+            });
         });
     }
 
-    createResponseRecord() {
-        const responseFields = {};
-
-        responseFields[SURVEY_FIELD.fieldApiName] = this.surveyId;
-        responseFields[RECORD_TYPE_ID_FIELD.fieldApiName] = this.getRecordTypeId('Survey Response');
-        responseFields[RATING_FIELD.fieldApiName] = this.rating;
-        responseFields[COMMENT_FIELD.fieldApiName] = this.comment;
-
-        const recordInput = {
-            apiName: SURVEY_RESPONSE_OBJECT.objectApiName,
-            fields: responseFields
-        };
-
-        createRecord(recordInput).then((record) => {
-            console.log('SurveyResponse is created: ', record);
+    handleCancel() {
+        this.show = false;
+        createAnsweredRecord({ surveyId: this.surveyId }).then((res) => {
+            console.log('Result of Survey Answered creation: ', res);
         });
     }
 }
