289746416ac3f5273c405b2da09a8abdee7b4257
eirikfladby <eirik.fladby@gmail.com>
2024-04-18 15:03:10 +0200

    new design button

---
+++ force-app/main/default/lwc/nksNavigateToNewApplication/nksNavigateToNewApplication.js
@@ -0,0 +1,37 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getUsersCurrentApp from '@salesforce/apex/NKS_HomePageController.getUsersCurrentApp';
+import updateUsersCurrentApp from '@salesforce/apex/NKS_HomePageController.updateUsersCurrentApp';
+
+export default class NksNavigateToNewApplication extends NavigationMixin(LightningElement) {
+    currentApp = 'NAV_Kontaktsenter';
+    connectedCallback() {
+        getUsersCurrentApp().then(res => {
+            this.currentApp = res;            
+        });
+    }
+
+    get nextApp() {
+        return this.currentApp === 'NAV_Kontaktsenter' ? 'c__NAV_Kontaktsenter_v_2' : 'c__NAV_Kontaktsenter';
+    }
+
+    // Alternative navigation is through window.open
+    /* 
+        let baseUrl = window.location.origin + '/lightning/';
+        let url = baseUrl + 'app/c__' + 'NAV_Kontaktsenter_v_2' + '/r/' + this.objectApiName + '/' + this.recordId + '/view';
+        window.open(url, '_self');
+    */
+    switchApplication() {
+        try {
+            this[NavigationMixin.Navigate]({
+                type: 'standard__app',
+                attributes: {
+                    appTarget: this.nextApp
+                }
+            });
+            updateUsersCurrentApp({ appTarget: this.nextApp.replace('c__', '').replace('standard__', '') });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
