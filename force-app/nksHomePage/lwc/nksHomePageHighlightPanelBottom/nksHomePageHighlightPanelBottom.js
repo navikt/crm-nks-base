aae515f31b79b71e54802247ca4b73d31567a569
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2024-10-22 08:36:21 +0200

    fix order

--- force-app/nksHomePage/lwc/nksHomePageHighlightPanelBottom/nksHomePageHighlightPanelBottom.js
+++ force-app/nksHomePage/lwc/nksHomePageHighlightPanelBottom/nksHomePageHighlightPanelBottom.js
@@ -3,12 +3,12 @@ export default class NksHomePageHighlightPanelBottom extends LightningElement {
     get links() {
         return [
             {
-                title: 'Norsk-engelsk ordliste',
-                url: 'https://navno.sharepoint.com/sites/enhet-kontaktsenter/SitePages/Ordliste-Norsk-Engelsk.aspx'
-            },
-            {
                 title: 'Kontaktsenterportalen',
                 url: 'https://navno.sharepoint.com/sites/enhet-kontaktsenter/'
+            },
+            {
+                title: 'Norsk-engelsk ordliste',
+                url: 'https://navno.sharepoint.com/sites/enhet-kontaktsenter/SitePages/Ordliste-Norsk-Engelsk.aspx'
             },
             {
                 title: 'Teknisk hjelp - IT',
