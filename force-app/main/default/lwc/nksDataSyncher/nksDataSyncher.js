d31a9a28849a128e2aa8d55f2cf5cb9a73bb4fe7
eirikfladby <eirik.fladby@gmail.com>
2025-02-12 13:29:13 +0100

    added new channel and refreshing custom components when synch is complete

--- force-app/main/default/lwc/nksDataSyncher/nksDataSyncher.js
+++ force-app/main/default/lwc/nksDataSyncher/nksDataSyncher.js
@@ -1,14 +1,17 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { publish, MessageContext } from 'lightning/messageService';
+import DATA_SYNC_CHANNEL from '@salesforce/messageChannel/DataSyncChannel__c';
 import getRelatedRecord from '@salesforce/apex/NksRecordInfoController.getRelatedRecord';
 import syncBankAccountNumber from '@salesforce/apex/NKS_DataSynchController.doBankAccountNumberSync';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import PERSON_IDENT_FIELD from '@salesforce/schema/Person__c.Name';
-import PERSON_ACTORID_FIELD from '@salesforce/schema/Person__c.INT_ActorId__c';
-import PERSON_ACCOUNT_FIELD from '@salesforce/schema/Person__c.CRM_Account__c';
 import { syncActorOppgaver } from 'c/crmOppgaveSyncher';
 import { resolve } from 'c/nksComponentsUtils';
 
-const syncStatus = {
+import PERSON_IDENT_FIELD from '@salesforce/schema/Person__c.Name';
+import PERSON_ACTORID_FIELD from '@salesforce/schema/Person__c.INT_ActorId__c';
+import PERSON_ACCOUNT_FIELD from '@salesforce/schema/Person__c.CRM_Account__c';
+
+const SYNC_STATUS = {
     SYNCING: 'SYNCING',
     SYNCED: 'SYNCED',
     ERROR: 'ERROR'
@@ -18,19 +21,26 @@ export default class NksDataSyncher extends LightningElement {
     @api objectApiName;
     @api relationshipField;
 
-    @track syncStatuses = [];
-
-    wireFields = [this.objectApiName + '.Id'];
+    syncStatuses = [];
     personId;
-    personFields = [PERSON_ACTORID_FIELD, PERSON_IDENT_FIELD, PERSON_ACCOUNT_FIELD];
     initialized = false;
     synced = false;
 
+    wireFields = [this.objectApiName + '.Id'];
+    personFields = [PERSON_ACTORID_FIELD, PERSON_IDENT_FIELD, PERSON_ACCOUNT_FIELD];
+
+    @wire(MessageContext)
+    messageContext;
+
     connectedCallback() {
         //Initial synch performed in connected callback to prevent boxcaring due to many events triggered at the same time.
-        this.addSyncStatus('bankAccount', 'Bankkontonummer', syncStatus.SYNCING);
-        this.addSyncStatus('oppgave', 'Oppgave', syncStatus.SYNCING);
-        this.getRelatedRecordId(this.relationshipField, this.objectApiName);
+        this.initializeSync();
+    }
+
+    initializeSync() {
+        this.addSyncStatus('bankAccount', 'Bankkontonummer', SYNC_STATUS.SYNCING);
+        this.addSyncStatus('oppgave', 'Oppgave', SYNC_STATUS.SYNCING);
+        this.getRelatedRecordId();
         this.initialized = true;
     }
 
@@ -41,8 +51,8 @@ export default class NksDataSyncher extends LightningElement {
     wiredRecordInfo({ error, data }) {
         if (data) {
             //Not called in wire context before initial synch has been done through connectedCallback
-            if (this.initialized === true && this.relationshipField && this.objectApiName) {
-                this.getRelatedRecordId(this.relationshipField, this.objectApiName);
+            if (this.initialized && this.relationshipField && this.objectApiName) {
+                this.getRelatedRecordId();
             }
         } else if (error) {
             console.log('Problme getting record: ', JSON.stringify(error, null, 2));
@@ -60,7 +70,7 @@ export default class NksDataSyncher extends LightningElement {
             let personAccountId = getFieldValue(data, PERSON_ACCOUNT_FIELD);
 
             if (personIdent) {
-                this.doSynch(personIdent, personActorId, personAccountId);
+                this.startSynch(personIdent, personActorId, personAccountId);
             }
         }
         if (error) {
@@ -68,62 +78,62 @@ export default class NksDataSyncher extends LightningElement {
         }
     }
 
-    async doSynch(personIdent, personActorId, eventName = 'e.force:refreshView') {
+    async startSynch(personIdent, personActorId, eventName = 'e.force:refreshView') {
         try {
             this.synced = false;
-            await Promise.all([this.bankAccountNumberSync(personIdent), this.oppgaveSync(personActorId)]);
+            await Promise.all([this.syncBankAccountNumber(personIdent), this.syncOppgave(personActorId)]);
             this.synced = true;
             const refreshEvent = new CustomEvent(eventName);
             this.dispatchEvent(refreshEvent);
+
+            // e.force:refreshView only works for standard components
+            // Notify other custom components that callout is complete to refresh data
+            publish(this.messageContext, DATA_SYNC_CHANNEL, { status: 'SYNC_COMPLETE' });
         } catch (error) {
             console.error('Problem synching bankAccountNumber/oppgave: ', JSON.stringify(error, null, 2));
         }
     }
 
-    async oppgaveSync(personActorId) {
-        try {
-            const syncStatusObj = this.getSyncStatus('oppgave');
-            if (syncStatusObj.status !== syncStatus.SYNCING) {
-                return;
-            }
+    async syncOppgave(personActorId) {
+        if (this.getSyncStatus('oppgave') !== SYNC_STATUS.SYNCING) return;
 
+        try {
             await syncActorOppgaver(personActorId);
-            this.setSyncStatus('oppgave', syncStatus.SYNCED);
+            this.setSyncStatus('oppgave', SYNC_STATUS.SYNCED);
         } catch (error) {
-            this.setSyncStatus('oppgave', syncStatus.ERROR);
-            console.error('Error in oppgaveSync:', JSON.stringify(error, null, 2));
+            this.setSyncStatus('oppgave', SYNC_STATUS.ERROR);
+            console.error('Error in syncOppgave:', JSON.stringify(error, null, 2));
             throw new Error('Error syncing oppgave: ' + error.message);
         }
     }
 
-    async bankAccountNumberSync(ident) {
-        try {
-            const syncStatusObj = this.getSyncStatus('bankAccount');
-            if (syncStatusObj.status !== syncStatus.SYNCING) {
-                return;
-            }
+    async syncBankAccountNumber(ident) {
+        if (this.getSyncStatus('bankAccount') !== SYNC_STATUS.SYNCING) return;
 
+        try {
             await syncBankAccountNumber({ ident: ident });
-            this.setSyncStatus('bankAccount', syncStatus.SYNCED);
+            this.setSyncStatus('bankAccount', SYNC_STATUS.SYNCED);
         } catch (error) {
-            this.setSyncStatus('bankAccount', syncStatus.ERROR);
-            console.error('Error in bankAccountNumberSync:', JSON.stringify(error, null, 2));
+            this.setSyncStatus('bankAccount', SYNC_STATUS.ERROR);
+            console.error('Error in syncBankAccountNumber:', JSON.stringify(error, null, 2));
             throw new Error('Error syncing bank account number: ' + error.message);
         }
     }
 
-    getRelatedRecordId(relationshipField, objectApiName) {
+    getRelatedRecordId() {
+        if (!this.recordId || !this.relationshipField || !this.objectApiName) return;
+
         getRelatedRecord({
             parentId: this.recordId,
-            relationshipField: relationshipField,
-            objectApiName: objectApiName
+            relationshipField: this.relationshipField,
+            objectApiName: this.objectApiName
         })
             .then((record) => {
-                let resolvedPersonId = resolve(relationshipField, record);
+                const resolvedPersonId = resolve(this.relationshipField, record);
                 //Only update the wired attribute if it is indeed changed
                 if (this.personId !== resolvedPersonId) {
-                    this.setSyncStatus('bankAccount', syncStatus.SYNCING);
-                    this.setSyncStatus('oppgave', syncStatus.SYNCING);
+                    this.setSyncStatus('bankAccount', SYNC_STATUS.SYNCING);
+                    this.setSyncStatus('oppgave', SYNC_STATUS.SYNCING);
                     this.personId = resolvedPersonId;
                 }
             })
@@ -133,17 +143,17 @@ export default class NksDataSyncher extends LightningElement {
     }
 
     addSyncStatus(name, label, status) {
-        let ss = this.getSyncStatus(name);
+        let syncStatus = this.getSyncStatus(name);
 
-        if (ss) {
-            ss.label = label;
-            ss.status = status;
+        if (syncStatus) {
+            syncStatus.label = label;
+            syncStatus.status = status;
         } else {
-            ss = this.getNewSyncStatus(name, label, status);
-            this.syncStatuses.push(ss);
+            syncStatus = this.getNewSyncStatus(name, label, status);
+            this.syncStatuses.push(syncStatus);
         }
 
-        this.calculateSyncStatus(ss);
+        this.calculateSyncStatus(syncStatus);
     }
 
     getNewSyncStatus(name, label, status) {
@@ -158,17 +168,17 @@ export default class NksDataSyncher extends LightningElement {
     }
 
     setSyncStatus(name, status) {
-        let ss = this.getSyncStatus(name);
-        ss.status = status;
-        if (ss) {
-            this.calculateSyncStatus(ss);
+        let syncStatus = this.getSyncStatus(name);
+        syncStatus.status = status;
+        if (syncStatus) {
+            this.calculateSyncStatus(syncStatus);
         }
     }
 
-    calculateSyncStatus(ss) {
-        ss.isSyncing = ss.status === syncStatus.SYNCING;
-        ss.isSynced = ss.status === syncStatus.SYNCED;
-        ss.isError = ss.status === syncStatus.ERROR;
+    calculateSyncStatus(syncStatus) {
+        syncStatus.isSyncing = syncStatus.status === SYNC_STATUS.SYNCING;
+        syncStatus.isSynced = syncStatus.status === SYNC_STATUS.SYNCED;
+        syncStatus.isError = syncStatus.status === SYNC_STATUS.ERROR;
     }
 
     getSyncStatus(name) {
