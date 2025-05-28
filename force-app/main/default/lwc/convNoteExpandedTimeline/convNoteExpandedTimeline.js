32be1a813163569013e37a6cb0962e5bf7b7d38f
Jan Robert Gjøstøl <jan.robert.gjostol@nav.no>
2025-05-28 14:05:01 +0200

    added print button

--- force-app/main/default/lwc/convNoteExpandedTimeline/convNoteExpandedTimeline.js
+++ force-app/main/default/lwc/convNoteExpandedTimeline/convNoteExpandedTimeline.js
@@ -48,6 +48,23 @@ export default class ConvNoteExpandedTimeline extends NavigationMixin(LightningE
         }
     }
 
+    handlePrint() {
+        console.log(this.recordId);
+        if (!this.recordId) {
+            console.error('No record ID found!');
+            return;
+        }
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__webPage',
+            attributes: {
+                url: `/apex/NKS_RecordPrint?id=${this.recordId}`
+            }
+        }).then((url) => {
+            console.log('Opening URL:', url);
+            window.open(url, '_self');
+        });
+    }
+
     get isLoading() {
         return !this.conversationNote && this.error === false;
     }
