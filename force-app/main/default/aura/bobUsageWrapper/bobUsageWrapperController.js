05cd37578bc01d1cfb756b10fc9550110e79b2a8
Vegard Sorheller <vegard.sorheller@nav.no>
2025-09-23 16:09:42 +0200

    Usage modal for bob statistics

--- force-app/main/default/aura/bobUsageWrapper/bobUsageWrapperController.js
+++ force-app/main/default/aura/bobUsageWrapper/bobUsageWrapperController.js
@@ -1,15 +1,66 @@
 ({
-    onTabClosed: function (component, event) {
-        var closedTabId = event.getParam('tabId');
-        var workspaceAPI = component.find('workspace');
-        const recordId = component.get('v.recordId');
-        const launcher = component.find('launcher');
+    onTabCreated: function (component, event) {
+        const workspaceAPI = component.find('workspace');
+        const tabId = event.getParam('tabId');
 
-        // Work around to avoide opening modal for subtabs. If getTabInfo returns data, it is a subtab becuse a tab still exists.
-        workspaceAPI.getTabInfo({ tabId: closedTabId }).catch(function () {
-            if (recordId && recordId.startsWith('500') && launcher && typeof launcher.openModal === 'function') {
-                launcher.openModal(recordId);
-            }
-        });
+        workspaceAPI
+            .getTabInfo({ tabId: tabId })
+            .then(function (tabInfo) {
+                const isSubtab = !!tabInfo.isSubtab;
+                const objectApiName =
+                    tabInfo.pageReference &&
+                    tabInfo.pageReference.attributes &&
+                    tabInfo.pageReference.attributes.objectApiName
+                        ? tabInfo.pageReference.attributes.objectApiName
+                        : null;
+                const recordId =
+                    tabInfo.recordId ||
+                    (tabInfo.pageReference && tabInfo.pageReference.state && tabInfo.pageReference.state.recordId) ||
+                    null;
+                let map = {};
+                try {
+                    map = JSON.parse(sessionStorage.getItem('tabInfoMap')) || {};
+                } catch (e) {
+                    map = {};
+                }
+
+                map[tabId] = {
+                    isSubtab: isSubtab,
+                    objectApiName: objectApiName,
+                    recordId: recordId
+                };
+                sessionStorage.setItem('tabInfoMap', JSON.stringify(map));
+            })
+            .catch(function (error) {
+                console.error('Error getting tabInfo for tabId:', tabId, error);
+            });
+    },
+
+    onTabClosed: function (component, event) {
+        const closedTabId = event.getParam('tabId');
+        const launcher = component.find('launcher');
+        let map = {};
+        try {
+            map = JSON.parse(sessionStorage.getItem('tabInfoMap')) || {};
+        } catch (e) {
+            map = {};
+        }
+        const info = map[closedTabId];
+
+        if (
+            info &&
+            info.recordId &&
+            info.isSubtab === false &&
+            info.objectApiName === 'Case' &&
+            launcher &&
+            typeof launcher.openModal === 'function'
+        ) {
+            launcher.openModal(info.recordId);
+        }
+
+        if (info) {
+            delete map[closedTabId];
+            sessionStorage.setItem('tabInfoMap', JSON.stringify(map));
+        }
     }
 });
