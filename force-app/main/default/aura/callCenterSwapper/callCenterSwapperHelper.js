eb8293aac1f17a9ce8aee20bdcf7821496cd72ae
Morten Mikalsen <morten.mikalsen@hotmail.com>
2026-05-28 12:57:02 +0200

    Add call center swapping component

---
+++ force-app/main/default/aura/callCenterSwapper/callCenterSwapperHelper.js
@@ -0,0 +1,22 @@
+({
+    switchCallCenter: function (component, targetCallCenterId) {
+        var action = component.get('c.swapCallCenter');
+        action.setParams({
+            callcenterId: targetCallCenterId
+        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                component.set('v.currentCallCenterId', targetCallCenterId);
+                component.set('v.isUnrecognizedCenter', false);
+                component.set('v.isDisabled', false);
+                var puzzelId = component.get('v.puzzelCallCenterId');
+                component.set('v.isToggled', targetCallCenterId === puzzelId);
+                console.log('Call center switched successfully.');
+            } else {
+                console.error('Failed to switch call center: ' + response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    }
+});
